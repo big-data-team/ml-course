ca145ca1c30bf9f2920b668f1c35f9ce8ffac209
korepasha <korepanova.natalia@gmail.com>
2019-05-25 11:22:52 +0300

    Create ml2019q2_validate_environment.py

---
+++ ml2019q2_validate_environment.py
@@ -0,0 +1,14 @@
+try:
+    import numpy
+    import scipy
+    import matplotlib
+    import seaborn
+    import xlrd
+    import pandas
+    import lightgbm
+    import keras
+except Exception:
+    print("some of the libraries are NOT installed properly")
+    raise
+
+print("you are ready for the course!")
