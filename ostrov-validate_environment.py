84582eb3527eee19e33694c82861c655443fbdd5
NickVeld <NickVeld@users.noreply.github.com>
2019-07-11 14:01:56 +0300

    ostrov-validate_environment.py added (#3)

---
+++ ostrov-validate_environment.py
@@ -0,0 +1,10 @@
+try:
+    import numpy
+    import scipy
+    import matplotlib
+    import seaborn
+    import xlrd
+    import pandas
+except Exception:
+    print("some of the libraries are NOT installed properly")
+    raise
