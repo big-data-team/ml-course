cc07b0df2c1ea3807e048ac6b65a503861e62db8
Alexey Dral <aadral@bigdatateam.org>
2024-03-27 15:09:47 +0500

    backup old validation scripts

--- ml2019_yessenov_validate_environment.py
+++ ml2019_yessenov_validate_environment.py
@@ -5,7 +5,10 @@ try:
     import scipy
     import matplotlib
     import seaborn
+    import xlrd
     import pandas
+    import xgboost
+    import keras
 except Exception:
     print("some of the libraries are NOT installed properly")
     raise
